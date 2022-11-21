b6fe2bb20dbb46698dd2589cfc0c8debcd3ae3a1
Leehamb99 <leehamb99@gmail.com>
2022-11-21 14:21:08 +0000

    components setup

---
+++ src/components/NamePlate/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NamePlate = () => {
+  return (
+    <div>NamePlate</div>
+  )
+}
+
+export default NamePlate
---
+++ src/components/index.js
@@ -0,0 +1,3 @@
+export { default as NavBar } from './NavBar'
+export { default as NamePlate } from './NamePlate'
+export { default as ProjectCard } from './ProjectCard'
