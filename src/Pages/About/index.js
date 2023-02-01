2e675d7dc66dc9bcd4d8ee90ab3bcf818ccb5709
Leehamb99 <leehamb99@gmail.com>
2023-02-01 18:43:46 +0000

    add new date

--- src/Pages/About/index.js
+++ src/Pages/About/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const About = () => {
   return (
-    <h1>Work in Progress 26/01/2023</h1>
+    <h1>Work in Progress 01/02/2023</h1>
   )
 }
 
--- src/Pages/Contact/index.js
+++ src/Pages/Contact/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <h1>Work in Progress 26/01/2023</h1>
+    <h1>Work in Progress 01/02/2023</h1>
   )
 }
 
